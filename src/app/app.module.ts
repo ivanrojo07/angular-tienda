3812ae902836b4314df07c2e7e8c8829686dda1c
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-04 22:54:33 -0500

    creando con ng generate y lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  item_list: string;
+  constructor() {
+    this.item_list = "";
+   }
+
+  ngOnInit(): void {
+  }
+
+}
