b356f35b27c70ad4375f0753a39b40755645add3
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-05 17:12:33 -0500

    Crear redirection, routelink y page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProductsComponent } from './products/products.component';
-import { Product } from './product.model';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes:Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'home', component:HomeComponent},
   {path:'contact', component:ContactComponent},
-  {path:'products', component:ProductsComponent}
+  {path:'products', component:ProductsComponent},
+  {path:'**', component:PageNotFoundComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
 import { SwiperModule } from "swiper/angular";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { SwiperModule } from "swiper/angular";
     HeaderComponent,
     FooterComponent,
     BannerComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
