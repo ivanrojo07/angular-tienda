1d24d129344b91913b3f2ff5cc7fdfed99bbab0b
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-06 11:13:34 -0500

    Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { ProductsComponent } from './products/products.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AdminGuard } from './admin.guard';
 
 const routes:Routes = [
   {path:'', component:LayoutComponent, children:[
     {path:'', redirectTo:'home', pathMatch:'full'},
     {path:'home', loadChildren: ()=>import('./home/home.module').then(m=>m.HomeModule)},
-    {path:'contact', component:ContactComponent},
+    {path:'contact', component:ContactComponent, canActivate:[AdminGuard]},
     {path:'products', component:ProductsComponent},
     {path:'products/:id', component:ProductDetailComponent},
     {path:'**', component:PageNotFoundComponent}
