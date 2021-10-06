548641f35deac01828acc451616a13ff98a66604
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-05 19:52:58 -0500

    Crear Modules compartidos y core

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProductsComponent } from './products/products.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes:Routes = [
-  {path:'', redirectTo:'home', pathMatch:'full'},
-  {path:'home', component:HomeComponent},
-  {path:'contact', component:ContactComponent},
-  {path:'products', component:ProductsComponent},
-  {path:'products/:id', component:ProductDetailComponent},
-  {path:'**', component:PageNotFoundComponent}
+  {path:'', component:LayoutComponent, children:[
+    {path:'', redirectTo:'home', pathMatch:'full'},
+    {path:'home', loadChildren: ()=>import('./home/home.module').then(m=>m.HomeModule)},
+    {path:'contact', component:ContactComponent},
+    {path:'products', component:ProductsComponent},
+    {path:'products/:id', component:ProductDetailComponent},
+    {path:'**', component:PageNotFoundComponent}
+  ]}
+  
 ]
 
 @NgModule({
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BannerComponent } from "./components/banner/banner.component";
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./components/home/home.component";
+import { SwiperModule } from "swiper/angular";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    declarations:[
+        BannerComponent,
+        HomeComponent,
+        
+    ],
+    imports:[
+        HomeRoutingModule,
+        CommonModule,
+        SwiperModule,
+        SharedModule
+    ]
+})
+
+export class HomeModule{
+    
+}
